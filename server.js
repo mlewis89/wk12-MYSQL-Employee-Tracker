e0b969370d81a80b4b4652ce1be83a86ef622e1a
Mark Lewis <mlewis89@gamil.com>
2024-03-29 09:18:08 +1100

    db connection

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const inquirer = require('inquirer');
+require('dotenv').config();
+const mysql =  require('mysql2');
 
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+},
+console.log(`Connected to the ${process.env.DB_NAME} database.`)
+)
