9dfcd5a0d5d6ac053f9f02c5c39664f410257a38
Mark Lewis <mlewis89@gamil.com>
2024-03-29 09:22:11 +1100

    fixed mismatch db names

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const mysql =  require('mysql2');
 
 // Connect to database
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-},
-console.log(`Connected to the ${process.env.DB_NAME} database.`)
-)
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log(`Connected to the ${process.env.DB_NAME} database.`)
+);
+
