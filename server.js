4008b202de564ad99160f7c6fc81465c9317dbf3
Mark Lewis <mlewis89@gmail.com>
2024-04-01 13:29:04 +1100

    removed sub functions

--- server.js
+++ server.js
@@ -52,179 +52,144 @@ const mainMenu = () => {
     .then((answers) => {
       switch (answers.action) {
         case "view all departments":
-          viewAll('department');
-          break;
+                                            //WHEN I choose to view all departments
+                                            //  THEN I am presented with a formatted table showing department names and department ids
+                                            db.query("SELECT * FROM department", function (err, results) {
+                                                console.table(results);
+                                            });
+                                            break;
         case "view all roles":
-            viewAll('role') ; 
-          break;
+                                            //WHEN I choose to view all roles
+                                            //  THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+                                            db.query("SELECT * FROM role", function (err, results) {
+                                                console.table(results);
+                                            });
+                                            break;
         case "view all employees":
-            viewAll('employee');
-          break;
+                                            //WHEN I choose to view all employees
+                                            //  THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+                                            db.query("SELECT * FROM employee", function (err, results) {
+                                                console.table(results);
+                                            });
+                                            break;
         case "add a department":
-          add_Department();
-          break;
+                                            //WHEN I choose to add a department
+                                            //  THEN I am prompted to enter the name of the department and that department is added to the database
+
+                                            inquirer
+                                                .prompt([
+                                                {
+                                                    //license,
+                                                    type: "Input",
+                                                    name: "name",
+                                                    message: "New Department name?",
+                                                },
+                                                ])
+                                                .then((answers) => {
+                                                //**todo** add new deparment to database
+                                                });
+                                            break;
         case "add a role":
-          add_Role();
-          break;
+                                            //WHEN I choose to add a role
+                                            //  THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+                                            let departments = [];
+                                            db.query("SELECT * FROM department", function (err, results) {
+                                                results.forEach((res) => departments.push(res.name));
+                                            });
+
+                                            inquirer
+                                                .prompt([
+                                                {
+                                                    type: "Input",
+                                                    name: "name",
+                                                    message: "New role name?",
+                                                },
+                                                {
+                                                    type: "Input",
+                                                    name: "salary",
+                                                    message: "what is the salary?",
+                                                },
+                                                {
+                                                    //license,
+                                                    type: "list",
+                                                    name: "department",
+                                                    message: "please select a department?",
+                                                    choices: departments,
+                                                },
+                                                ])
+                                                .then((answers) => {
+                                                //**todo** add new role to database
+                                                });
+                                            break;
         case "add an employee":
-          add_Employee();
-          break;
+                                                                                roles = [];
+                                                                                managers = [];
+
+                                                                                db.query("SELECT * FROM role", function (err, results) {
+                                                                                    results.forEach((res) => roles.push(res.name));
+                                                                                });
+
+                                                                                db.query("SELECT * FROM employee", function (err, results) {
+                                                                                    results.forEach((res) => managers.push(res.name));
+                                                                                });
+
+                                                                                console.log(roles);
+                                                                                console.log(managers);
+
+                                                                                inquirer
+                                                                                    .prompt([
+                                                                                    {
+                                                                                        type: "Input",
+                                                                                        name: "firstName",
+                                                                                        message: "First Name?",
+                                                                                    },
+                                                                                    {
+                                                                                        type: "Input",
+                                                                                        name: "lastName",
+                                                                                        message: "Last Name?",
+                                                                                    },
+                                                                                    {
+                                                                                        type: "list",
+                                                                                        name: "role",
+                                                                                        message: "please select a role?",
+                                                                                        choices: roles,
+                                                                                    },
+                                                                                    {
+                                                                                        type: "list",
+                                                                                        name: "manager",
+                                                                                        message: "please select a manager?",
+                                                                                        choices: managers,
+                                                                                    },
+                                                                                    ])
+                                                                                    .then((answers) => {
+                                                                                    //**todo** add new role to database
+                                                                                    });
+                                                                                break;
         case "update an employee role":
-          update_Employee();
-          break;
+                                                //WHEN I choose to update an employee role
+                                                //  THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+
+                                                employees = [];
+                                                db.query("SELECT * FROM employee", function (err, results) {
+                                                    results.forEach((res) => employees.push(res.name));
+                                                });
+
+                                                inquirer
+                                                    .prompt([
+                                                    {
+                                                        type: "list",
+                                                        name: "employee",
+                                                        message: "please select an employee?",
+                                                        choices: employees,
+                                                    },
+                                                    ])
+                                                    .then((answers) => {
+                                                    //**todo** add new role to database
+                                                    });
+                                                break;
       }
-    })
-};
-
-const viewAll = (table) => {
-    db.query(`SELECT * FROM ${table}`,(err, results) => {
-      console.table(results);
-    });
-    mainMenu();
-  };
-
-//WHEN I choose to view all departments
-//  THEN I am presented with a formatted table showing department names and department ids
-const viewAll_Departments = () => {
-  db.query("SELECT * FROM department", function (err, results) {
-    console.table(results);
-  });
-};
-
-//WHEN I choose to view all roles
-//  THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-const viewAll_Roles = () => {
-  db.query("SELECT * FROM role", function (err, results) {
-    console.table(results);
-  });
-};
-
-//WHEN I choose to view all employees
-//  THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-const viewAll_Employees = () => {
-  db.query("SELECT * FROM employee", function (err, results) {
-    console.table(results);
-  });
-};
-
-//WHEN I choose to add a department
-//  THEN I am prompted to enter the name of the department and that department is added to the database
-const add_Department = () => {
-  inquirer
-    .prompt([
-      {
-        //license,
-        type: "Input",
-        name: "name",
-        message: "New Department name?",
-      },
-    ])
-    .then((answers) => {
-      //**todo** add new deparment to database
-    });
-};
-
-//WHEN I choose to add a role
-//  THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-const add_Role = () => {
-  //TODO - popuplate from database;
-  let departments = [];
-  db.query("SELECT * FROM department", function (err, results) {
-    results.forEach((res) => departments.push(res.name));
-  });
-
-  inquirer
-    .prompt([
-      {
-        type: "Input",
-        name: "name",
-        message: "New role name?",
-      },
-      {
-        type: "Input",
-        name: "salary",
-        message: "what is the salary?",
-      },
-      {
-        //license,
-        type: "list",
-        name: "department",
-        message: "please select a department?",
-        choices: departments,
-      },
-    ])
-    .then((answers) => {
-      //**todo** add new role to database
-    });
-};
-
-//WHEN I choose to add an employee
-//  THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-const add_Employee = () => {
-  //TODO - popuplate from database;
-  roles = [];
-  managers = [];
-
-  db.query("SELECT * FROM role", function (err, results) {
-    results.forEach((res) => roles.push(res.name));
-  });
-
-  db.query("SELECT * FROM employee", function (err, results) {
-    results.forEach((res) => managers.push(res.name));
-  });
-
-  console.log(roles);
-  console.log(managers);
-
-  inquirer
-    .prompt([
-      {
-        type: "Input",
-        name: "firstName",
-        message: "First Name?",
-      },
-      {
-        type: "Input",
-        name: "lastName",
-        message: "Last Name?",
-      },
-      {
-        type: "list",
-        name: "role",
-        message: "please select a role?",
-        choices: roles,
-      },
-      {
-        type: "list",
-        name: "manager",
-        message: "please select a manager?",
-        choices: managers,
-      },
-    ])
-    .then((answers) => {
-      //**todo** add new role to database
-    });
-};
-
-//WHEN I choose to update an employee role
-//  THEN I am prompted to select an employee to update and their new role and this information is updated in the database
-const update_Employee = () => {
-  employees = [];
-  db.query("SELECT * FROM employee", function (err, results) {
-    results.forEach((res) => employees.push(res.name));
-  });
-
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "employee",
-        message: "please select an employee?",
-        choices: employees,
-      },
-    ])
-    .then((answers) => {
-      //**todo** add new role to database
     });
 };
 
